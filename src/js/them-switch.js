9a0a92594fbebca3d69e299847a6f727195c0c08
Artem451 <75530269+Artem451@users.noreply.github.com>
2021-06-18 20:37:35 +0400

    Update them-switch.js

--- src/js/them-switch.js
+++ src/js/them-switch.js
@@ -7,11 +7,16 @@ const Theme = {
 
   const bodyEl = document.querySelector('body')
   const themeSwitchButton = document.querySelector('#theme-switch-toggle')
+  
+  bodyEl.classList.add (localStorage.getItem('theme'))
+  themeSwitchButton.setAttribute("checked", localStorage.getItem('cheked')) 
   themeSwitchButton.addEventListener('change', themeSwitch)
-  bodyEl.classList.add(Theme.LIGHT)
-
+  
+  
   function themeSwitch (evnt) {
-    
+    const savedTheme = bodyEl.classList
+    const savedCheck = evnt.target.checked
+    console.log(evnt.target.checked)
     if(evnt.target.checked) {
        bodyEl.classList.add(Theme.DARK)
        bodyEl.classList.remove(Theme.LIGHT)
@@ -19,4 +24,8 @@ const Theme = {
         bodyEl.classList.add(Theme.LIGHT)
         bodyEl.classList.remove(Theme.DARK)
     }
+    localStorage.setItem('theme', savedTheme)
+    localStorage.setItem('cheked', savedCheck)
   }
+  
+  
