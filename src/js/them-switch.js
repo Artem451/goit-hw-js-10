5401d4e6173642e0b4b2bdf75909deb815a59d9e
Artem451 <75530269+Artem451@users.noreply.github.com>
2021-06-18 23:10:27 +0400

    Update them-switch.js

--- src/js/them-switch.js
+++ src/js/them-switch.js
@@ -9,29 +9,34 @@ const Theme = {
   const themeSwitchButton = document.querySelector('#theme-switch-toggle')
   const marker = document.querySelector('.theme-switch__track')
 
-  console.log(marker.attributes)
-  console.log(marker.getAttribute('isConnected'))
 
-  bodyEl.classList.add (localStorage.getItem('theme'))
-  
   themeSwitchButton.addEventListener('change', themeSwitch)
+  themeSwitchButton.addEventListener('change', checkBoxSave)
+  bodyEl.classList.add (localStorage.getItem('theme'))
+  themeSwitchButton.checked = localStorage.getItem('cheked')
   
   function themeSwitch (evnt) {
+    event.preventDefault()
     const savedTheme = bodyEl.classList
-    const savedCheck = evnt.target.checked
-
-    if(evnt.target.checked) {
-       bodyEl.classList.add(Theme.DARK)
-       bodyEl.classList.remove(Theme.LIGHT)
+    
+    
+    if(!evnt.target.checked) {
+       bodyEl.classList.add(Theme.LIGHT)
+       bodyEl.classList.remove(Theme.DARK)
        
     } else {
-        bodyEl.classList.add(Theme.LIGHT)
-        bodyEl.classList.remove(Theme.DARK)
-        
+        bodyEl.classList.add(Theme.DARK)
+        bodyEl.classList.remove(Theme.LIGHT)
     }
     localStorage.setItem('theme', savedTheme)
-    localStorage.setItem('cheked', savedCheck)
-
   }
+
+  function checkBoxSave (evnt) {
+    const savedCheck = evnt.target.checked
+    localStorage.setItem('checked', evnt.target.checked)
+    if (localStorage.getItem('cheked')) {
+        return true
+    } else {return false}
+  } 
   
   
